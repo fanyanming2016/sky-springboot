2703d8a3457c244e2c9e3b7296056c533153c2e8
fanyanming <beer2100@qq.com>
2020-06-12 15:22:52 +0800

    add metrics demo

--- src/main/java/springboot/controller/HelloWorldController.java
+++ src/main/java/springboot/controller/HelloWorldController.java
@@ -1,12 +1,35 @@
 package springboot.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.PostConstruct;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    MeterRegistry registry;
+
+    // Prometheus Count 数据类型
+    private Counter requests;
+
+    @PostConstruct
+    private void init(){
+        // 定义 指标名 和 标签
+        requests = registry.counter("requests_total", "method", "sayHello");
+    }
+
     @RequestMapping("/")
     public String sayHello() {
-        return "Hello,SkyWalking!";
+        // # 开始统计访问次数
+        try{
+            requests.increment();
+        } catch (Exception e) {
+            return e.toString();
+        }
+        return "Hello,SkyWalking! visit " + requests.count();
     }
 }
